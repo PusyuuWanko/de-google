2cf35ad5814f09455bb3a33938127335e1fd2f83
PusyuuOfficial <121094209+PusyuuOfficial@users.noreply.github.com>
2023-02-25 16:43:34 +0900

    Add files via upload

---
+++ スマホGoogleアプリ、キャリアアプリ無効化.bat
@@ -0,0 +1,128 @@
+Google 日本語入力
+adb shell pm uninstall -k --user 0 com.google.android.inputmethod.japanese
+
+Google Podcasts
+adb shell pm uninstall -k --user 0 com.google.android.apps.podcasts
+adb shell pm uninstall -k --user 0 com.google.android.apps.podcasts.PodcastsActivity
+Device Health Services
+adb shell pm uninstall -k --user 0 com.google.android.apps.turbo
+
+Digital Wellbeing
+adb shell pm uninstall -k --user 0 com.google.android.apps.wellbeing
+
+YouTube Music
+adb shell pm uninstall -k --user 0 com.google.android.apps.youtube.music
+adb shell pm uninstall -k --user 0 com.google.android.apps.youtube.music.activities.MusicActivity
+Android の設定
+adb shell pm uninstall -k --user 0 com.google.android.setupwizard
+
+Google Play Services for AR
+adb shell pm uninstall -k --user 0 com.google.ar.core
+
+adb shell pm uninstall -k --user 0 com.google.android.dialer
+
+adb shell pm uninstall -k --user 0 com.google.android.contacts
+
+adb shell pm uninstall -k --user 0 com.google.android.apps.messaging
+
+Android の設定
+adb shell pm uninstall -k --user 0 com.google.android.apps.restore
+
+Google Pay
+adb shell pm uninstall -k --user 0 com.google.android.apps.walletnfcrel
+adb shell pm uninstall -k --user 0 com.google.commerce.tapandpay.android.cardlist.CardListActivity
+
+Google Partner Setup
+adb shell pm uninstall -k --user 0 com.google.android.partnersetup
+
+Google の音声サービス
+adb shell pm uninstall -k --user 0 com.google.android.tts
+
+Google コンタクト
+adb shell pm uninstall -k --user 0 com.google.android.contacts
+
+adb shell pm uninstall -k --user 0 sharp.jp.android.makersiteappli.activity.Launcher
+Google Play ストア
+adb shell pm uninstall -k --user 0 com.android.vending
+adb shell pm uninstall -k --user 0 com.android.vending.AssetBrowserActivity
+Files by Google
+adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.files
+adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.files.home.HomeActivity
+Chrome
+adb shell pm uninstall -k --user 0 com.android.chrome
+adb shell pm uninstall -k --user 0 com.google.android.apps.chrome.Main
+フォト
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos.home.HomeActivity
+ドライブ
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs.app.NewMainProxyActivity
+
+YouTube
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+adb shell pm uninstall -k --user 0 com.google.android.youtube.app.honeycomb.Shell$HomeActivity
+Google One
+adb shell pm uninstall -k --user 0 com.google.android.apps.subscriptions.red
+adb shell pm uninstall -k --user 0 com.google.android.apps.subscriptions.red.LauncherActivity
+Googleサービスフレームワーク
+adb shell pm uninstall -k --user 0 com.google.android.gsf
+
+マップ
+adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+adb shell pm uninstall -k --user 0 com.google.android.maps.MapsActivity
+Duo
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon.MainActivity
+
+Google Play 開発者サービス
+adb shell pm uninstall -k --user 0 com.google.android.gms
+
+Google
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox.SearchActivity
+Google TV
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.google.android.videos.GoogleTvEntryPoint
+
+Android ユーザー補助設定ツール
+adb shell pm uninstall -k --user 0 com.google.android.marvin.talkback
+
+Android Auto
+adb shell pm uninstall -k --user 0 com.google.android.projection.gearhead
+
+Google Play services for payments
+adb shell pm uninstall -k --user 0 com.google.android.gms.pay.sidecar
+
+Googleの連絡先の同期
+adb shell pm uninstall -k --user 0 com.google.android.syncadapters.contacts
+
+Google One Time Init
+adb shell pm uninstall -k --user 0 com.google.android.onetimeinitializer
+
+Gmail
+adb shell pm uninstall -k --user 0 com.google.android.gm
+adb shell pm uninstall -k --user 0 com.google.android.gm.ConversationListActivityGmail
+電卓
+adb shell pm uninstall -k --user 0 com.google.android.calculator
+adb shell pm uninstall -k --user 0 com.android.calculator2.Calculator
+Google ニュース
+adb shell pm uninstall -k --user 0 com.google.android.apps.magazines
+adb shell pm uninstall -k --user 0 com.google.apps.dots.android.app.activity.CurrentsStartActivity
+Gboard
+adb shell pm uninstall -k --user 0 com.google.android.inputmethod.latin
+
+Google ロケーション履歴
+adb shell pm uninstall -k --user 0 com.google.android.gms.location.history
+
+ConfigUpdater
+adb shell pm uninstall -k --user 0 com.google.android.configupdater
+
+アシスタント
+adb shell pm uninstall -k --user 0 com.google.android.apps.googleassistant
+adb shell pm uninstall -k --user 0 com.google.android.apps.googleassistant.AssistantActivity
+マーケットフィードバックエージェント
+adb shell pm uninstall -k --user 0 com.google.android.feedback
+
+カレンダー
+adb shell pm uninstall -k --user 0 com.google.android.calendar
+adb shell pm uninstall -k --user 0 com.android.calendar.AllInOneActivity
