acab70a751f311d3e3f98aab68da34595990450e
PusyuuOfficial <121094209+PusyuuOfficial@users.noreply.github.com>
2023-02-25 16:51:22 +0900

    Add files via upload

---
+++ KYV43.bat
@@ -0,0 +1,190 @@
+使い方練習
+adb shell pm uninstall -k --user 0 com.kddi.practice_kyv43
+adb shell pm uninstall -k --user 0 com.kddi.practice_kyv43.MainActivity
+Wowma! for au
+adb shell pm uninstall -k --user 0 com.kddi.auoneshopping
+adb shell pm uninstall -k --user 0 jp.wowma.app.view.activity.StartActivity
+電話
+adb shell pm uninstall -k --user 0 com.android.dialer
+adb shell pm uninstall -k --user 0 com.android.dialer.DialtactsActivity
+YouTube
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+adb shell pm uninstall -k --user 0 com.google.android.youtube.app.honeycomb.Shell$HomeActivity
+カーナビデータ通信設定
+adb shell pm uninstall -k --user 0 com.kddi.android.btdun
+
+Google Play Music
+adb shell pm uninstall -k --user 0 com.google.android.music
+adb shell pm uninstall -k --user 0 com.android.music.activitymanagement.TopLevelActivity
+セルフケア設定
+adb shell pm uninstall -k --user 0 com.kddi.selfcare.client
+
+Duo
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon.MainActivity
+ウォーキングNAVITIME-simple-
+adb shell pm uninstall -k --user 0 com.navitime.local.alkoo.simple
+adb shell pm uninstall -k --user 0 com.navitime.local.alkoo.simple.NTMainNavitime
+テレビde写真
+adb shell pm uninstall -k --user 0 jp.kyocera.TVConnect
+adb shell pm uninstall -k --user 0 jp.kyocera.TVConnect.operationselect.OperationActivity
+SMS
+adb shell pm uninstall -k --user 0 com.kddi.android.cmail
+adb shell pm uninstall -k --user 0 com.kddi.android.cmail.ui.list.ThreadListActivity
+Google
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox.SearchActivity
+External Service
+adb shell pm uninstall -k --user 0 com.kddi.selfcare.external
+
+auメール
+adb shell pm uninstall -k --user 0 com.kddi.android.auemail
+adb shell pm uninstall -k --user 0 com.kddi.android.auemail.MainActivity
+セットアップウィザード
+adb shell pm uninstall -k --user 0 com.google.android.setupwizard
+
+Chrome
+adb shell pm uninstall -k --user 0 com.android.chrome
+adb shell pm uninstall -k --user 0 com.google.android.apps.chrome.Main
+au Wi-Fi接続ツール
+adb shell pm uninstall -k --user 0 com.kddi.android.au_wifi_connect2
+adb shell pm uninstall -k --user 0 jp.co.netvision.WifiConnect.WifiConnectManual
+サービスTOP
+adb shell pm uninstall -k --user 0 com.kddi.pass.launcher
+adb shell pm uninstall -k --user 0 com.kddi.pass.launcher.Launch
+Facebook
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm uninstall -k --user 0 com.facebook.katana.LoginActivity
+迷惑メッセージ・電話ブロック
+adb shell pm uninstall -k --user 0 com.tobila.tobilaphone.mobile.android.au
+adb shell pm uninstall -k --user 0 com.tobila.tobilaphone.mobile.android.activity.TPSplashActivity
+マーケットフィードバックエージェント
+adb shell pm uninstall -k --user 0 com.google.android.feedback
+
+お問い合わせ
+adb shell pm uninstall -k --user 0 com.kddi.cs.app001.contact
+adb shell pm uninstall -k --user 0 com.kddi.co.app001.contact.MainActivity
+au WALLET Market
+adb shell pm uninstall -k --user 0 web.wm.auone.jp
+adb shell pm uninstall -k --user 0 web.wm.auone.jp.SplashActivity
+Google One Time Init
+adb shell pm uninstall -k --user 0 com.google.android.onetimeinitializer
+
+データお預かり
+adb shell pm uninstall -k --user 0 com.kddi.android.mamoru
+adb shell pm uninstall -k --user 0 com.kddi.android.mamoru.activities.LaunchActivity
+File Commander
+adb shell pm uninstall -k --user 0 com.mobisystems.fileman
+adb shell pm uninstall -k --user 0 com.mobisystems.files.FileBrowser
+au災害対策
+adb shell pm uninstall -k --user 0 com.kddi.disasterapp
+adb shell pm uninstall -k --user 0 com.kddi.disasterapp.activity.ActivityTopScreen
+Google Playムービー＆TV
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.google.android.youtube.videos.EntryPoint
+Googleの連絡先の同期
+adb shell pm uninstall -k --user 0 com.google.android.syncadapters.contacts
+
+OfficeSuite
+adb shell pm uninstall -k --user 0 com.mobisystems.office
+adb shell pm uninstall -k --user 0 com.mobisystems.office.splashScreen.SplashScreenActivity
+Googleカレンダーの同期
+adb shell pm uninstall -k --user 0 com.google.android.syncadapters.calendar
+
+フォト
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos.home.HomeActivity
+取扱説明書
+adb shell pm uninstall -k --user 0 kyocera.kyv43.onlinemanual
+adb shell pm uninstall -k --user 0 kyocera.kyv43.onlinemanual.MainActivity
+My au
+adb shell pm uninstall -k --user 0 com.kddi.cs.app001
+adb shell pm uninstall -k --user 0 com.kddi.cs.app001.Agreement
+Google Play 開発者サービス
+adb shell pm uninstall -k --user 0 com.google.android.gms
+
+Google バックアップ トランスポート
+adb shell pm uninstall -k --user 0 com.google.android.backuptransport
+
+ConfigUpdater
+adb shell pm uninstall -k --user 0 com.google.android.configupdater
+
+遠隔操作サポート
+adb shell pm uninstall -k --user 0 jp.co.optim.oru
+
+マップ
+adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+adb shell pm uninstall -k --user 0 com.google.android.maps.MapsActivity
+
+au通信品質レポート機能
+adb shell pm uninstall -k --user 0 com.kddi.android.checker_android
+
+au設定メニュー
+adb shell pm uninstall -k --user 0 com.kddi.android.au_setting_menu
+
+ウイルスバスター for au
+adb shell pm uninstall -k --user 0 com.trendmicro.tmmssuite.oemkddi.jp
+adb shell pm uninstall -k --user 0 com.trendmicro.tmmssuite.consumer.login.ui.Login
+Facebook App Installer
+adb shell pm uninstall -k --user 0 com.facebook.system
+
+みてね
+adb shell pm uninstall -k --user 0 us.mitene
+adb shell pm uninstall -k --user 0 us.mitene.app.startup.ui.StartupActivity
+auｽﾏｰﾄﾊﾟｽ
+adb shell pm uninstall -k --user 0 com.kddi.android.smartpass
+adb shell pm uninstall -k --user 0 com.kddi.android.smartpass.Main
+ドライブ
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs.app.NewMainProxyActivity
+au初期設定
+adb shell pm uninstall -k --user 0 com.kddi.android.auinitialsetting
+
+au位置情報サービス
+adb shell pm uninstall -k --user 0 com.kddi.android.klop
+
+Gmail
+adb shell pm uninstall -k --user 0 com.google.android.gm
+adb shell pm uninstall -k --user 0 com.google.android.gm.ConversationListActivityGmail
+故障紛失サポート
+adb shell pm uninstall -k --user 0 com.kddi.android.repairreceipt
+adb shell pm uninstall -k --user 0 com.kddi.android.repairreceipt.MainActivity
+ニュースパス
+adb shell pm uninstall -k --user 0 com.kddi.android.newspass
+adb shell pm uninstall -k --user 0 com.kddi.android.newspass.activity.MainActivity
+au Market
+adb shell pm uninstall -k --user 0 com.kddi.market
+adb shell pm uninstall -k --user 0 com.kddi.market.activity.ActivityMain
+Facebook App Manager
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+
+使いこなし動画
+adb shell pm uninstall -k --user 0 jp.kyocera.videoformaster
+adb shell pm uninstall -k --user 0 jp.kyocera.videoformaster.MainActivity
+Google Play ストア
+adb shell pm uninstall -k --user 0 com.android.vending
+adb shell pm uninstall -k --user 0 com.android.vending.AssetBrowserActivity
+au WALLET
+adb shell pm uninstall -k --user 0 jp.auone.wallet
+adb shell pm uninstall -k --user 0 jp.auone.wallet.activity.AppStartActivity
+使い方ｶﾞｲﾄﾞ
+adb shell pm uninstall -k --user 0 jp.kyocera.sitek
+adb shell pm uninstall -k --user 0 jp.kyocera.sitek.SiteKActivity
+あんしんフィルター for au
+adb shell pm uninstall -k --user 0 jp.netstar.familysmile
+adb shell pm uninstall -k --user 0 jp.netstar.familysmile.MainActivity
+Googleサービスフレームワーク
+adb shell pm uninstall -k --user 0 com.google.android.gsf
+
+Googleパートナーのセットアップ
+adb shell pm uninstall -k --user 0 com.google.android.partnersetup
+
+Music Player (LISMO)
+adb shell pm uninstall -k --user 0 com.kddi.android.imp
+adb shell pm uninstall -k --user 0 com.kddi.android.imp.activity.IMPLibraryGroup
+Googleアカウントマネージャー
+adb shell pm uninstall -k --user 0 com.google.android.gsf.login
+
+アプリケーションadb shell pm uninstall -k --user 0 スマホの健康診断
+adb shell pm uninstall -k --user 0 jp.kyocera.selfcheck
+adb shell pm uninstall -k --user 0 jp.kyocera.selfcheck.SelfCheckMainActivity
---
+++ oukitel-googleServiceDisable.bat
@@ -0,0 +1,102 @@
+adb uninstall -k --user 0 com.google.android.apps.maps
+adb uninstall -k --user 0 com.google.android.maps.MapsActivity
+
+adb uninstall -k --user 0 com.google.android.apps.wellbeing
+
+adb uninstall -k --user 0 com.google.android.apps.tachyon
+adb uninstall -k --user 0 com.google.android.apps.tachyon.MainActivity
+
+adb uninstall -k --user 0 com.google.android.ims
+
+adb uninstall -k --user 0 com.google.android.apps.youtube.music
+adb uninstall -k --user 0 com.google.android.apps.youtube.music.activities.MusicActivity
+
+adb uninstall -k --user 0 com.google.android.setupwizard
+
+adb uninstall -k --user 0 com.google.android.gms
+
+adb uninstall -k --user 0 android.auto_generated_rro_vendor__
+
+adb uninstall -k --user 0 com.google.android.apps.messaging
+adb uninstall -k --user 0 com.google.android.apps.messaging.ui.ConversationListActivity
+
+adb uninstall -k --user 0 com.google.android.googlequicksearchbox
+adb uninstall -k --user 0 com.google.android.googlequicksearchbox.SearchActivity
+
+adb uninstall -k --user 0 com.google.android.videos
+adb uninstall -k --user 0 com.google.android.videos.GoogleTvEntryPoint
+
+adb uninstall -k --user 0 com.google.android.contacts
+adb uninstall -k --user 0 com.android.contacts.activities.PeopleActivity
+
+adb uninstall -k --user 0 com.google.android.marvin.talkback
+
+adb uninstall -k --user 0 com.google.android.projection.gearhead
+
+adb uninstall -k --user 0 com.google.android.apps.restore
+
+adb uninstall -k --user 0 com.google.android.partnersetup
+
+adb uninstall -k --user 0 com.google.android.safetycenter.resources
+
+adb uninstall -k --user 0 com.google.android.tts
+
+adb uninstall -k --user 0 com.android.vending
+adb uninstall -k --user 0 com.android.vending.AssetBrowserActivity
+
+adb uninstall -k --user 0 com.google.android.apps.nbu.files
+adb uninstall -k --user 0 com.google.android.apps.nbu.files.home.HomeActivity
+
+adb uninstall -k --user 0 com.android.chrome
+adb uninstall -k --user 0 com.google.android.apps.chrome.Main
+
+adb uninstall -k --user 0 com.google.android.syncadapters.contacts
+
+adb uninstall -k --user 0 com.google.android.apps.photos
+adb uninstall -k --user 0 com.google.android.apps.photos.home.HomeActivity
+
+adb uninstall -k --user 0 com.google.android.dialer
+adb uninstall -k --user 0 com.google.android.dialer.extensions.GoogleDialtactsActivity
+
+adb uninstall -k --user 0 android.auto_generated_rro_product__
+
+adb uninstall -k --user 0 com.google.android.onetimeinitializer
+
+adb uninstall -k --user 0 com.google.android.gm
+adb uninstall -k --user 0 com.google.android.gm.ConversationListActivityGmail
+
+adb uninstall -k --user 0 com.google.android.calculator
+adb uninstall -k --user 0 com.android.calculator2.Calculator
+
+adb uninstall -k --user 0 com.google.android.apps.docs
+adb uninstall -k --user 0 com.google.android.apps.docs.app.NewMainProxyActivity
+
+adb uninstall -k --user 0 com.google.android.inputmethod.latin
+
+adb uninstall -k --user 0 com.google.android.networkstack.overlay
+
+adb uninstall -k --user 0 com.google.android.youtube
+adb uninstall -k --user 0 com.google.android.youtube.app.honeycomb.Shell$HomeActivity
+
+adb uninstall -k --user 0 com.google.android.gms.location.history
+
+adb uninstall -k --user 0 com.google.android.gmsintegration
+
+adb uninstall -k --user 0 com.google.android.apps.googleassistant
+adb uninstall -k --user 0 com.google.android.apps.googleassistant.AssistantActivity
+
+adb uninstall -k --user 0 com.google.android.gsf
+
+adb uninstall -k --user 0 com.google.android.apps.setupwizard.searchselector
+
+adb uninstall -k --user 0 com.google.android.calendar
+adb uninstall -k --user 0 com.android.calendar.AllInOneActivity
+
+adb uninstall -k --user 0 com.google.android.gmsintegration
+
+adb uninstall -k --user 0 com.google.android.gsf
+
+adb uninstall -k --user 0 com.google.android.apps.setupwizard.searchselector
+
+adb uninstall -k --user 0 com.google.android.calendar
+adb uninstall -k --user 0 com.android.calendar.AllInOneActivity
